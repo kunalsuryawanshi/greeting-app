353b6fa7e7abb6108e9ec01aff3117bea0e5cddd
kunalsuryawanshi <kunalsuryawanshi25@gmail.com>
2021-12-03 16:35:58 +0530

    Added: first commit

---
+++ src/main/java/com/bridgelabz/greetingapp/GreetingAppApplication.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.greetingapp;
+/**
+ * Purpose: To Simulate With Greeting App
+ *
+ * @author KUNAL SURYAWANSHI
+ * @version 2.6.0
+ * @since 30-11-2021
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingAppApplication.class, args);
+    }
+
+}
