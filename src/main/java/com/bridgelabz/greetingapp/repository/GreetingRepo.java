d429db0e3126722666ec02ba3ff4c2c4a1bea19c
kunalsuryawanshi <kunalsuryawanshi25@gmail.com>
2021-12-06 03:26:49 +0530

    Added: Code for Delete Message By ID

--- src/main/java/com/bridgelabz/greetingapp/repository/GreetingRepo.java
+++ src/main/java/com/bridgelabz/greetingapp/repository/GreetingRepo.java
@@ -3,6 +3,8 @@ package com.bridgelabz.greetingapp.repository;
 import com.bridgelabz.greetingapp.entity.GreetingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface GreetingRepo extends JpaRepository<GreetingEntity, Integer> {
 }
